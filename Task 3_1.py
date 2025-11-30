e8a2f57f42e5cd1c33fa826f3e562770fafe896a
sanjaych90 <sanjayiam@outlook.com>
2025-11-30 22:05:13 +0530

    Code revision

--- Task 3_1.py
+++ Task 3_1.py
@@ -1,7 +1,7 @@
 exceptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", ",", "/", "\\", "[", "]", "{", "}", "+", "=", "_", "(", ")", "*", "&", "^", "<", ">", "%", "$", "#", "@", "!", "`", "~", "'", ":", '"', " "]
 
 flag = True
-num = input("Enter a number: ")
+
 for i in num:
     if i not in numbers:
         print("Please enter only integers")
