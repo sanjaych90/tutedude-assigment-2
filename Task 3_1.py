ad1b4c48cb28818587576513e49b7c985aba616e
sanjaych90 <sanjayiam@outlook.com>
2025-11-30 19:51:42 +0530

    Initial commit

---
+++ Task 3_1.py
@@ -0,0 +1,16 @@
+exceptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", ",", "/", "\\", "[", "]", "{", "}", "+", "=", "_", "(", ")", "*", "&", "^", "<", ">", "%", "$", "#", "@", "!", "`", "~", "'", ":", '"', " "]
+
+flag = True
+num = input("Enter a number: ")
+for i in num:
+    if i in exceptions:
+        print("Please enter only integers")
+        flag = False
+        break
+
+if flag:
+    num = int(num)
+    if num % 2 == 0:
+        print(f"{num} is an even number")
+    else:
+        print(f"{num} is an odd number")
---
+++ Task 3_2.py
@@ -0,0 +1,5 @@
+total = 0
+for num in range(1, 51):
+    total = total + num
+
+print(f"The sum of numbers from 1 to 50 is: {total}")
